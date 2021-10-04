beea2efbea3ad77d81e49cf62d22b4d59c7f7196
David Veselý <dawe.vesely@gmail.com>
2021-10-04 23:30:10 +0200

    Add basic header

---
+++ components/Header.vue
@@ -0,0 +1,63 @@
+<template>
+  <header>
+    <div class="container pt-12 flex justify-between">
+      <LogoOm />
+
+      <nav class="flex items-center">
+        <nuxt-link
+          v-for="(item, index) in menuItems"
+          :key="index"
+          :to="item.link"
+          class="NavItem uppercase text-sm text-primary font-semibold ml-10 hover:underline"
+          v-text="item.text"
+        />
+      </nav>
+    </div>
+  </header>
+</template>
+
+<script>
+import LogoOm from '~/assets/img/logo-om.svg?inline';
+
+export default {
+  components: {
+    LogoOm,
+  },
+  data() {
+    return {
+      menuItems: [
+        {
+          link: '/',
+          text: 'O nás',
+        },
+        {
+          link: '/',
+          text: 'Spolupráce',
+        },
+        {
+          link: '/',
+          text: 'Projekty',
+        },
+        {
+          link: '/',
+          text: 'Členové',
+        },
+        {
+          link: '/',
+          text: 'Aktuality',
+        },
+        {
+          link: '/',
+          text: 'Kontakt',
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.NavItem.is-active {
+  @apply text-secondary;
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,16 @@ module.exports = {
     extend: {
       screens: {
         'hover-none': { raw: '(hover: none)' },
-        xs:{ max: '350px' },
+        xs: { max: '350px' },
       },
-      // colors: {
-      //   'primary': 'var(--color-primary)',
-      // },
+      colors: {
+        primary: '#1E1796',
+        secondary: '#00D48F',
+        additional: '#FFCC00',
+      },
     },
   },
   plugins: [
-    // require('@tailwindcss/typography'),
+    require('@tailwindcss/typography'),
   ],
 };
