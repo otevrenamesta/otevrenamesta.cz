fa03f92a841a5b74bed118dc6f090a3ad582ec17
David Veselý <dawe.vesely@gmail.com>
2021-10-05 01:00:49 +0200

    dummy commit

--- pages/index.vue
+++ pages/index.vue
@@ -16,8 +16,8 @@
 
 <script>
 export default {
-  async mounted() {
-    console.log(await this.$content('hello').fetch());
-  },
+  // async mounted() {
+  //   console.log(await this.$content('hello').fetch());
+  // },
 };
 </script>
