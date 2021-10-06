ee24a788b306f136ca270c57ef578ed5e017a88e
David Veselý <dawe.vesely@gmail.com>
2021-10-06 23:05:36 +0200

    Add sections to collaboration page

---
+++ components/collaboration/CollaborationSection.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container flex">
+    <div class="relative">
+      <Grid
+        :columns="6"
+        :rows="7"
+      />
+      <component
+        :is="illustration"
+        class="absolute top-block-2 left-block-2"
+      />
+    </div>
+    <div class="p-block-1.5 pt-block-1 pb-0">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+import IllustrationCollaboration1 from '~/assets/img/illustration-collaboration-1.svg?inline';
+import IllustrationCollaboration2 from '~/assets/img/illustration-collaboration-2.svg?inline';
+import IllustrationCollaboration3 from '~/assets/img/illustration-collaboration-3.svg?inline';
+
+export default {
+  components: {
+    IllustrationCollaboration1,
+    IllustrationCollaboration2,
+    IllustrationCollaboration3,
+  },
+  props: {
+    illustration: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
--- pages/collaboration.vue
+++ pages/collaboration.vue
@@ -7,6 +7,208 @@
     <CollaborationIntro
       class="mb-block-2"
     />
+
+    <CollaborationSection
+      illustration="illustration-collaboration-1"
+    >
+      <div class="max-w-lg">
+        <h2 class="text-5xl text-primary font-bold tracking-tight mb-5">
+          Stát se členem
+        </h2>
+        <p class="text-secondary text-base font-medium mb-block-1">
+          Lorem ipsum pecializujeme se na Technickou, právní a manažerskou expertízu v oblasti digitalizace samospráv. Máme bohaté zkušenosti s budováním aplikací na míru pro samosprávní celky
+        </p>
+        <h3 class="text-lg text-priamry uppercase text-primary font-bold mb-5">
+          Členský poplatek
+        </h3>
+        <p class="text-sm text-primary mb-block-0.5">
+          Členský poplatek se vypočítává jako 1,50 kč na osobu z přepočtu obyvatel v oblasti digitalizace samospráv. Máme bohaté zkušenosti s budováním aplikací na míru pro samosprávní celky
+        </p>
+      </div>
+      <h3 class="text-lg text-priamry uppercase text-primary font-bold mb-5">
+        Benefity
+      </h3>
+      <div class="flex flex-wrap -mx-block-0.5">
+        <div class="w-1/2">
+          <div class="px-block-0.5 mb-block-0.5">
+            <strong class="block text-sm font-semibold text-primary text-opacity-75 mb-2.5">
+              Rozhodování o činnosti spolku
+            </strong>
+            <p class="text-black text-opacity-60 text-sm font-medium">
+              Consectetuer adipiscing elit. Nulla quis diam. Praesent in mauris eu tortor porttitor accumsan.
+            </p>
+          </div>
+        </div>
+        <div class="w-1/2">
+          <div class="px-block-0.5 mb-block-0.5">
+            <strong class="block text-sm font-semibold text-primary text-opacity-75 mb-2.5">
+              Sdílené know-how
+            </strong>
+            <p class="text-black text-opacity-60 text-sm font-medium">
+              Consectetuer adipiscing elit. Nulla quis diam. Praesent in mauris eu tortor porttitor accumsan.
+            </p>
+          </div>
+        </div>
+        <div class="w-1/2">
+          <div class="px-block-0.5 mb-block-0.5">
+            <strong class="block text-sm font-semibold text-primary text-opacity-75 mb-2.5">
+              Slevy na produktech
+            </strong>
+            <p class="text-black text-opacity-60 text-sm font-medium">
+              Consectetuer adipiscing elit. Nulla quis diam. Praesent in mauris eu tortor porttitor accumsan.
+            </p>
+          </div>
+        </div>
+        <div class="w-1/2">
+          <div class="px-block-0.5 mb-block-1">
+            <strong class="block text-sm font-semibold text-primary text-opacity-75 mb-2.5">
+              Vulnerability scan
+            </strong>
+            <p class="text-black text-opacity-60 text-sm font-medium">
+              Consectetuer adipiscing elit. Nulla quis diam. Praesent in mauris eu tortor porttitor accumsan.
+            </p>
+          </div>
+        </div>
+      </div>
+    </CollaborationSection>
+
+    <CollaborationSection
+      illustration="illustration-collaboration-2"
+    >
+      <div class="max-w-lg">
+        <h2 class="text-5xl text-primary font-bold tracking-tight mb-5">
+          Nákup služeb
+        </h2>
+        <p class="text-secondary text-base font-medium mb-block-1">
+          Lorem ipsum pecializujeme se na Technickou, právní a manažerskou expertízu v oblasti digitalizace samospráv. Máme bohaté zkušenosti s budováním aplikací na míru pro samosprávní celky
+        </p>
+        <h3 class="text-lg text-priamry uppercase text-primary font-bold mb-5">
+          Ceník
+        </h3>
+        <p class="text-sm text-primary mb-block-0.5">
+          Ceník se dělí na consectetuer adipiscing elit. Nulla quis diam. Praesent in mauris eu tortor porttitor accumsan.
+        </p>
+        <div class="flex items-center mb-block-1">
+          <Button
+            type="transparent"
+            icon="icon-arrow-right"
+            class="text-secondary pl-0"
+          >
+            Ceník pro členy
+          </Button>
+          <Button
+            type="transparent"
+            icon="icon-arrow-right"
+            class="text-secondary"
+          >
+            ceník pro ostatní
+          </Button>
+        </div>
+      </div>
+      <h3 class="text-lg text-priamry uppercase text-primary font-bold mb-5">
+        Benefity
+      </h3>
+      <div class="flex flex-wrap -mx-block-0.5">
+        <div class="w-1/2">
+          <div class="px-block-0.5 mb-block-1">
+            <strong class="block text-sm font-semibold text-primary text-opacity-75 mb-2.5">
+              Rozhodování o činnosti spolku
+            </strong>
+            <p class="text-black text-opacity-60 text-sm font-medium">
+              Consectetuer adipiscing elit. Nulla quis diam. Praesent in mauris eu tortor porttitor accumsan.
+            </p>
+          </div>
+        </div>
+        <div class="w-1/2">
+          <div class="px-block-0.5 mb-block-1">
+            <strong class="block text-sm font-semibold text-primary text-opacity-75 mb-2.5">
+              Sdílené know-how
+            </strong>
+            <p class="text-black text-opacity-60 text-sm font-medium">
+              Consectetuer adipiscing elit. Nulla quis diam. Praesent in mauris eu tortor porttitor accumsan.
+            </p>
+          </div>
+        </div>
+        <div class="w-1/2">
+          <div class="px-block-0.5 mb-block-1">
+            <strong class="block text-sm font-semibold text-primary text-opacity-75 mb-2.5">
+              Slevy na produktech
+            </strong>
+            <p class="text-black text-opacity-60 text-sm font-medium">
+              Consectetuer adipiscing elit. Nulla quis diam. Praesent in mauris eu tortor porttitor accumsan.
+            </p>
+          </div>
+        </div>
+        <div class="w-1/2">
+          <div class="px-block-0.5 mb-block-1">
+            <strong class="block text-sm font-semibold text-primary text-opacity-75 mb-2.5">
+              Vulnerability scan
+            </strong>
+            <p class="text-black text-opacity-60 text-sm font-medium">
+              Consectetuer adipiscing elit. Nulla quis diam. Praesent in mauris eu tortor porttitor accumsan.
+            </p>
+          </div>
+        </div>
+      </div>
+    </CollaborationSection>
+
+    <CollaborationSection
+      illustration="illustration-collaboration-3"
+    >
+      <div class="max-w-lg">
+        <h2 class="text-5xl text-primary font-bold tracking-tight mb-5">
+          Využití zdrojových kódů
+        </h2>
+        <p class="text-secondary text-base font-medium mb-block-1">
+          Lorem ipsum pecializujeme se na Technickou, právní a manažerskou expertízu v oblasti digitalizace samospráv. Máme bohaté zkušenosti s budováním aplikací na míru pro samosprávní celky
+        </p>
+      </div>
+      <h3 class="text-lg text-priamry uppercase text-primary font-bold mb-5">
+        Benefity
+      </h3>
+      <div class="flex flex-wrap -mx-block-0.5">
+        <div class="w-1/2">
+          <div class="px-block-0.5 mb-block-1">
+            <strong class="block text-sm font-semibold text-primary text-opacity-75 mb-2.5">
+              Rozhodování o činnosti spolku
+            </strong>
+            <p class="text-black text-opacity-60 text-sm font-medium">
+              Consectetuer adipiscing elit. Nulla quis diam. Praesent in mauris eu tortor porttitor accumsan.
+            </p>
+          </div>
+        </div>
+        <div class="w-1/2">
+          <div class="px-block-0.5 mb-block-1">
+            <strong class="block text-sm font-semibold text-primary text-opacity-75 mb-2.5">
+              Sdílené know-how
+            </strong>
+            <p class="text-black text-opacity-60 text-sm font-medium">
+              Consectetuer adipiscing elit. Nulla quis diam. Praesent in mauris eu tortor porttitor accumsan.
+            </p>
+          </div>
+        </div>
+        <div class="w-1/2">
+          <div class="px-block-0.5 mb-block-1">
+            <strong class="block text-sm font-semibold text-primary text-opacity-75 mb-2.5">
+              Slevy na produktech
+            </strong>
+            <p class="text-black text-opacity-60 text-sm font-medium">
+              Consectetuer adipiscing elit. Nulla quis diam. Praesent in mauris eu tortor porttitor accumsan.
+            </p>
+          </div>
+        </div>
+        <div class="w-1/2">
+          <div class="px-block-0.5 mb-block-1">
+            <strong class="block text-sm font-semibold text-primary text-opacity-75 mb-2.5">
+              Vulnerability scan
+            </strong>
+            <p class="text-black text-opacity-60 text-sm font-medium">
+              Consectetuer adipiscing elit. Nulla quis diam. Praesent in mauris eu tortor porttitor accumsan.
+            </p>
+          </div>
+        </div>
+      </div>
+    </CollaborationSection>
   </main>
 </template>
 
