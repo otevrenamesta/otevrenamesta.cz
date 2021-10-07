24a4b9e3641997a9aee0fac44b29e72e54f82359
David Veselý <dawe.vesely@gmail.com>
2021-10-07 18:19:29 +0200

    Add members page

---
+++ components/members/MembersMember.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="flex items-center">
+    <nuxt-img
+      :src="member.image"
+      class="w-1/2"
+    />
+    <div class="px-block-0.5 w-1/2">
+      <h2 class="text-xl font-bold text-primary mb-5">
+        {{ member.fullname }}
+      </h2>
+      <p class="mb-4">
+        <span class="text-secondary text-[12px] uppercase">
+          Zástupce
+        </span>
+        <br>
+        <strong class="text-sm font-bold">
+          {{ member.agent }}
+        </strong>
+      </p>
+      <p class="mb-4">
+        <span class="text-secondary text-[12px] uppercase">
+          Počet obyvatel
+        </span>
+        <br>
+        <strong class="text-sm font-bold">
+          {{ member.population }}
+        </strong>
+      </p>
+      <p class="mb-4">
+        <span class="text-secondary text-[12px] uppercase">
+          Členský poplatek
+        </span>
+        <br>
+        <strong class="text-sm font-bold">
+          {{ member.memberFee }}
+        </strong>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    member: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
---
+++ pages/members.vue
@@ -0,0 +1,83 @@
+<template>
+  <main>
+    <MembersHero
+      class="mb-block-2"
+    />
+
+    <div class="container">
+      <h2 class="text-primary-dark font-bold text-4xl mb-8">
+        Členové
+      </h2>
+      <div class="flex flex-wrap -mx-block-1">
+        <div
+          v-for="(member, index) in members"
+          :key="index"
+          class="w-1/3"
+        >
+          <MembersMember
+            :member="member"
+            class="px-block-1 mb-block-1"
+          />
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      members: [
+        {
+          image: 'https://picsum.photos/seed/picsum/480/480',
+          fullname: 'Název člena',
+          agent: 'Jaroslav Pokorný',
+          population: '182 000',
+          memberFee: '1 234 Kč / měsíc',
+        },
+        {
+          image: 'https://picsum.photos/seed/picsum/480/480',
+          fullname: 'Název člena',
+          agent: 'Jaroslav Pokorný',
+          population: '182 000',
+          memberFee: '1 234 Kč / měsíc',
+        },
+        {
+          image: 'https://picsum.photos/seed/picsum/480/480',
+          fullname: 'Název člena',
+          agent: 'Jaroslav Pokorný',
+          population: '182 000',
+          memberFee: '1 234 Kč / měsíc',
+        },
+        {
+          image: 'https://picsum.photos/seed/picsum/480/480',
+          fullname: 'Název člena',
+          agent: 'Jaroslav Pokorný',
+          population: '182 000',
+          memberFee: '1 234 Kč / měsíc',
+        },
+        {
+          image: 'https://picsum.photos/seed/picsum/480/480',
+          fullname: 'Název člena',
+          agent: 'Jaroslav Pokorný',
+          population: '182 000',
+          memberFee: '1 234 Kč / měsíc',
+        },
+        {
+          image: 'https://picsum.photos/seed/picsum/480/480',
+          fullname: 'Název člena',
+          agent: 'Jaroslav Pokorný',
+          population: '182 000',
+          memberFee: '1 234 Kč / měsíc',
+        },
+      ],
+    };
+  },
+  head() {
+    return {
+      title: `Členové ${this.$config.appendTitle}`,
+    };
+  },
+};
+</script>
