61ffb17bf4994666e78274773d89573b80950d37
David Veselý <dawe.vesely@gmail.com>
2021-10-30 12:16:26 +0100

    Responsive: collaboration

--- components/collaboration/CollaborationSection.vue
+++ components/collaboration/CollaborationSection.vue
@@ -1,17 +1,17 @@
 <template>
-  <div class="container flex flex-col lg:flex-row">
+  <div class="container flex flex-col lg:flex-row -mt-px">
     <div class="relative">
       <Grid
         :columns="$screen.xl ? 6 : ($screen.lg ? 5 : false)"
-        :rows="$screen.xl ? 7 : ($screen.lg ? 8 : 5)"
+        :rows="($screen.xl ? 7 : ($screen.lg ? 8 : 5)) + (illustration === 'illustration-collaboration-2' ? +2 : 0)"
       />
       <component
         :is="illustration"
         class="absolute top-block-2 left-block-2 h-auto"
-        :class="$screen.xl ? 'w-block-8' : 'w-block-6'"
+        :class="$screen.xl && illustration !== 'illustration-collaboration-2' ? 'w-block-8' : 'w-block-6'"
       />
     </div>
-    <div class="p-block-0.5 xl:p-block-1.5 pt-block-1 pb-0">
+    <div class="p-block-0.5 xl:p-block-1.5 pt-block-1 pb-0 xl:pb-0">
       <slot />
     </div>
   </div>
