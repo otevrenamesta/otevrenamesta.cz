5531751fce4f085f28cdc6369ef80f152f67a726
David Veselý <dawe.vesely@gmail.com>
2021-11-15 10:20:53 +0100

    Fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/no-v-html': 'off',
+    'vue/multi-word-component-names': 'off',
     'vue/singleline-html-element-content-newline': ['warn', {
       ignoreWhenNoAttributes: true,
       ignoreWhenEmpty: true,
