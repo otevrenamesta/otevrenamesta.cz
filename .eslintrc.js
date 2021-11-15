de4828d9c51c43fc5c6027cd6950e9aec482890a
David Veselý <dawe.vesely@gmail.com>
2021-11-15 10:19:48 +0100

    Upgrade npm deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,13 +19,6 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'vue/no-v-html': 'off',
-    'vue/max-attributes-per-line': ['error', {
-      singleline: 3,
-      multiline: {
-        max: 1,
-        allowFirstLine: false,
-      },
-    }],
     'vue/singleline-html-element-content-newline': ['warn', {
       ignoreWhenNoAttributes: true,
       ignoreWhenEmpty: true,
