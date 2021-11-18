e7ac3253beb14f80213f9616fc1186ad7da1d2ef
David Veselý <dawe.vesely@gmail.com>
2021-11-18 11:46:51 +0100

    Hide other projects

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -7,12 +7,12 @@
     </div>
     <Projects />
 
-    <div class="container mt-block-2">
+    <!-- <div class="container mt-block-2">
       <h2 class="text-primary font-bold text-4xl mb-block-1 tracking-tight">
         Další projekty
       </h2>
       <OtherProjets />
-    </div>
+    </div> -->
   </main>
 </template>
 
