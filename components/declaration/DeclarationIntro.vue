f157bc43416b211154e68b6082d6364fc81b9614
David Veselý <jsem@davidvesely.cz>
2023-09-22 16:39:37 +0200

    add optional chainings

--- components/declaration/DeclarationIntro.vue
+++ components/declaration/DeclarationIntro.vue
@@ -2,7 +2,7 @@
   <section class="pb-block-2">
     <div
       class="text-primary font-semibold text-3xl mb-block-1"
-      v-html="intro.title"
+      v-html="intro?.title"
     />
 
     <div class="flex flex-col sm:flex-row gap-4 mb-block-2">
@@ -12,7 +12,7 @@
         <Button
           icon="icon-arrow-open"
         >
-          {{ intro.cta.title }}
+          {{ intro.cta?.title }}
         </Button>
       </a>
       <Button
@@ -21,23 +21,23 @@
         icon="icon-arrow-right"
         class="text-secondary"
       >
-        {{ intro.signatoriesButton.title }}
+        {{ intro.signatoriesButton?.title }}
       </Button>
     </div>
 
     <div>
       <h3 class="uppercase text-primary text-xl font-bold mb-block-0.5">
-        {{ intro.opensource.title }}
+        {{ intro.opensource?.title }}
       </h3>
       <div
         class="text-xl prose mb-block-1"
-        v-html="intro.opensource.description"
+        v-html="intro.opensource?.description"
       />
 
       <div class="prose before:prose-li:bg-primary mb-block-1.5">
         <ul class="max-w-md">
           <li
-            v-for="(item, index) in intro.opensource.list"
+            v-for="(item, index) in intro.opensource?.list"
             :key="index"
             class="mb-block-0.5 !before:bg-secondary text-xl"
           >
@@ -47,13 +47,13 @@
       </div>
 
       <a
-        :href="intro.opensource.showMore.url"
+        :href="intro.opensource.showMore?.url"
         target="_blank"
       >
         <Button
           icon="icon-arrow-open"
         >
-          {{ intro.opensource.showMore.title }}
+          {{ intro.opensource.showMore?.title }}
         </Button>
       </a>
     </div>
