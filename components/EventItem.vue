32f9f0fe17d9f8ed7d74d21eb7679a901d5e86fb
David Veselý <jsem@davidvesely.cz>
2025-02-27 10:21:52 +0100

    Add czechospo page

---
+++ components/EventItem.vue
@@ -0,0 +1,64 @@
+<template>
+  <nuxt-link
+    :to="$localePath(`/events/${props.event.id}`)"
+    class="block w-full sm:w-1/2 md:w-1/3 max-w-md sm:max-w-full mb-block-2"
+  >
+    <div class="px-block-0.5">
+      <nuxt-img
+        :src="props.event.image.replace('?mode=crop&center=0.5,0.5&rnd=132199332150000000&width=575', '')"
+        :alt="props.event.title"
+        width="600"
+        height="400"
+        class="mb-block-0.5"
+      />
+      <div
+        class="mb-4 flex"
+      >
+        <div
+          v-for="(tag, tagIndex) in props.event.tags?.split(',')"
+          :key="tagIndex"
+          class="py-1.5 px-2.5 mr-2.5 bg-secondary rounded-3xl leading-none text-[12px] uppercase text-black/75 font-bold"
+        >
+          {{ tag }}
+        </div>
+      </div>
+      <h3 class="text-primary font-bold text-2xl tracking-tight mb-2 hover:underline">
+        {{ props.event.title }}
+      </h3>
+      <p class="text-primary font-medium">
+        {{ props.event.perex }}
+      </p>
+
+      <div class="grid grid-cols-2 gap-4 items-center mt-8">
+        <div class="flex items-center">
+          <IconCalendar class="w-4 h-4 mr-4" />
+          <time
+            :datetime="props.event.date"
+            class="block text-sm text-dark"
+          >
+            {{ props.event.date }}
+          </time>
+        </div>
+
+        <div class="flex items-center">
+          <IconLocation class="w-4 h-4 mr-4" />
+          <span class="text-sm text-dark">
+            {{ props.event.location }}
+          </span>
+        </div>
+      </div>
+    </div>
+  </nuxt-link>
+</template>
+
+<script setup>
+import IconCalendar from '~/assets/img/icon-calendar.svg';
+import IconLocation from '~/assets/img/icon-location.svg';
+
+const props = defineProps({
+  event: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
---
+++ components/czech-ospo/CzechOspoHero.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <section class="flex items-end relative pt-block-3 overflow-x-hidden w-full">
+      <div class="container relative flex flex-col justify-start items-start">
+        <div class="absolute z-10 top-0 h-full flex flex-col pt-block-2 xs:left-block-0.5 left-block-1.5 md:left-block-4">
+          <strong class="block text-secondary uppercase font-bold text-base leading-none h-block-1 md:h-block-2 -mt-0.5">
+            {{ hero.title }}
+          </strong>
+          <h2
+            v-preposition-space
+            class="text-primary text-2xl md:text-5xl md:leading-tight font-bold tracking-tighter h-block-2"
+            v-html="hero.description"
+          />
+          <div
+            class="text-primary w-11/12 lg:w-7/12 text-xl font-semibold"
+            v-html="hero.content"
+          />
+        </div>
+        <div class="flex flex-col justify-start items-start relative w-auto">
+          <div
+            class="w-block-2 sm:w-block-2 h-block-2 sm:h-block-2  absolute top-0 left-0 flex flex-wrap"
+          >
+            <div class="w-1/2 h-1/2 bg-primary" />
+            <div class="w-1/2 h-1/2 bg-primary" />
+            <div class="w-1/2 h-1/2 bg-transparent" />
+            <div class="w-1/2 h-1/2 bg-primary" />
+          </div>
+          <div
+            class="w-block-2 sm:w-block-4 h-block-2 sm:h-block-4 bg-secondary absolute top-0 right-0 sm:-right-block-2 rounded-br-full"
+          />
+          <div
+            class="w-block-2 sm:w-block-4 h-block-2 sm:h-block-4 bg-additional absolute left-0 sm:-left-block-2 bottom-0 rounded-full"
+          />
+          <div
+            class="w-block-2 h-block-2 bg-primary absolute bottom-block-2 right-0 hidden lg:block"
+          />
+          <Grid
+            :rows="$grid?.sm ? 6 : 10"
+          />
+        </div>
+      </div>
+    </section>
+    <div class="block md:hidden mt-10 px-6">
+      <img
+        src="~/assets/img/declaration-hero-logos.png"
+        alt="Logo"
+        class="max-w-full"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    hero() {
+      return useContentStore().czechOspo.hero;
+    },
+  },
+};
+</script>
