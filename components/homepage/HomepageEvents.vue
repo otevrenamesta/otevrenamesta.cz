ad191556be9f6eacdbcf23afbe58fa547754a71f
David Veselý <jsem@davidvesely.cz>
2025-05-14 10:29:57 +0200

    Update HomepageEvents template to conditionally render events

--- components/homepage/HomepageEvents.vue
+++ components/homepage/HomepageEvents.vue
@@ -1,28 +1,32 @@
 <template>
-  <section class="container pt-block-2">
-    <h3 class="uppercase text-primary text-xl font-bold mb-2">
-      {{ content?.title }}
-    </h3>
-    <nuxt-link
-      :to="$localePath('/events')"
-      class="block mb-block-0.5"
-    >
-      <Button
-        type="transparent"
-        icon="icon-arrow-right"
-        class="text-secondary"
+  <section
+    class="container pt-block-2"
+  >
+    <template v-if="eventsFiltered.length > 0">
+      <h3 class="uppercase text-primary text-xl font-bold mb-2">
+        {{ content?.title }}
+      </h3>
+      <nuxt-link
+        :to="$localePath('/events')"
+        class="block mb-block-0.5"
       >
-        {{ content?.allEvents }}
-      </Button>
-    </nuxt-link>
+        <Button
+          type="transparent"
+          icon="icon-arrow-right"
+          class="text-secondary"
+        >
+          {{ content?.allEvents }}
+        </Button>
+      </nuxt-link>
 
-    <div class="flex flex-wrap -mx-block-0.5">
-      <EventItem
-        v-for="event in eventsFiltered"
-        :key="event.id"
-        :event="event"
-      />
-    </div>
+      <div class="flex flex-wrap -mx-block-0.5">
+        <EventItem
+          v-for="event in eventsFiltered"
+          :key="event.id"
+          :event="event"
+        />
+      </div>
+    </template>
   </section>
 </template>
 
