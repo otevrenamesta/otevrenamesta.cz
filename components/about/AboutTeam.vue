f0c80c4be5fe05dbdf7b649b52dd13d3f72fcf05
Jan Volmut <jan.volmut@otevrenamesta.cz>
2026-07-14 14:59:30 +0200

    Updated about page

--- components/about/AboutTeam.vue
+++ components/about/AboutTeam.vue
@@ -15,21 +15,5 @@
         />
       </div>
     </div>
-
-    <h2 class="text-secondary font-bold text-4xl mb-8 mt-block-1.5">
-      {{ useContentStore().about.team.projectBoard }}
-    </h2>
-    <div class="flex flex-wrap -mx-6 md:-mx-block-0.5">
-      <div
-        v-for="(perosn, index) in useContentStore().about.employees"
-        :key="index"
-        class="px-6 md:px-block-0.5 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5"
-      >
-        <Person
-          :person="perosn"
-          class="text-secondary mb-block-1"
-        />
-      </div>
-    </div>
   </section>
 </template>
